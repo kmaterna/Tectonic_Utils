451ad87fdbd0f53e096ad5cbe900a2eb9713a4d9
kmaterna <kathrynmaterna@gmail.com>
2019-12-20 22:19:19 -0800

    read_kml simple function

---
+++ read_kml.py
@@ -0,0 +1,26 @@
+# The purpose of this code is to read a simple box drawn in Google Earth and saved as a KML file. 
+# The return value is a list of latitude and longitude coordinates. 
+
+import numpy as np 
+
+
+def read_simple_kml(infile):
+	print("Reading %s into arrays of lon and lat..." % infile)
+	start=0; lats=[]; lons=[];
+	ifile=open(infile,'r');
+	for line in ifile:
+		if start==1:
+			temp=line.split()
+			for item in temp:
+				lons.append(float(item.split(',')[0]))
+				lats.append(float(item.split(',')[1]))
+			break;
+		if "coordinates" in line:
+			start=1;
+	return lons, lats;
+
+
+
+if __name__=="__main__":
+	ifile="test1.kml"
+	lons,lats = read_simple_kml(ifile);
