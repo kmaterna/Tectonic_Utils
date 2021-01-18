7ef7124d7d0ec9c22990eaf39c650de16658fdad
kmaterna <kathrynmaterna@gmail.com>
2021-01-18 09:12:41 -0800

    bookkeeping for v0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="Tectonic_Utils", 
-    version="0.0.2",
+    version="0.0.3",
     author="Kathryn Materna",
     author_email="kmaterna@berkeley.edu",
     description="A small package of useful geophysics utilities",
