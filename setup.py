4ed24b0458feac2ad9bb80ed2a16cbaad1a1ce33
kmaterna <kathrynmaterna@gmail.com>
2025-04-14 22:02:56 -0600

    updating version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="Tectonic_Utils", 
-    version="0.1.0",
+    version="0.1.1",
     author="Kathryn Materna",
     author_email="kmaterna@berkeley.edu",
     description="A small package of useful geophysics utilities",
